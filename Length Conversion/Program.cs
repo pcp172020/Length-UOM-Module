9fdd4abb9b6f607824aac6947a63631e7247acd3
pcp172020 <pablopalacios@student.purdueglobal.edu>
2023-06-07 01:02:48 -0500

    Add project files.

---
+++ Length Conversion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Length_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inches: ");
+            int calculating = int.Parse(Console.ReadLine());
+            inches(calculating);
+            Console.ReadKey();
+        }
+
+        private static void inches(int calculating)
+        {
+            Console.WriteLine(calculating + " inches = " + 2.54 * calculating + " cm");
+            return;
+        }
+
+    }
+
+    class CMtoIN
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Centimeters ");
+            int calculating = int.Parse(Console.ReadLine());
+            centimeters(calculating);
+            Console.ReadKey();
+        }
+
+        private static void centimeters(int calculating)
+        {
+            Console.WriteLine(calculating + "centimeters = " + 0.39370 * calculating + " in");
+            return;
+        }
+    }
+}
